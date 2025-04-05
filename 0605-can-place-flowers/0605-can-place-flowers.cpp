3fcce3c1de722378a0209b497bd2634c76921954
Maaz Karim <103533417+Maaz-24503@users.noreply.github.com>
2025-04-05 20:10:44 +0500

    Time: 27 ms (4.69%), Space: 24.2 MB (6.67%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -3,7 +3,7 @@ public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
         int k = flowerbed.size();
         for(int i = 0; i<k; i++){
-            if((i == 0 || flowerbed[i-1] == 0) && (i == k-1 || flowerbed[i+1] == 0) && (flowerbed[i] == 0)) {
+            if(flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == k-1 || flowerbed[i+1] == 0)) {
                 cout<<i<<" ";
                 n--;
                 i++;
