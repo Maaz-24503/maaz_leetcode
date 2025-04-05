6012343f4dec1e7b51826be0bdd0e632a4c65d9e
Maaz Karim <103533417+Maaz-24503@users.noreply.github.com>
2025-04-05 17:56:25 +0500

    Time: 2 ms (28.72%), Space: 9.8 MB (30.68%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -1,29 +1,11 @@
 class Solution {
-private:
-    string str1, str2;
-    int n1, n2;
-
-    bool isValid(int m){
-        if(m > n1) return false;
-        if(n1 % m != 0 || n2 % m != 0) return false;
-        for(int i = 0; i<n1; i += m){
-            for(int j = 0; j<m; j++) if(str1[j] != str1[i+j]) return false;
-        }
-        for(int i = 0; i<n2; i += m){
-            for(int j = 0; j<m; j++) if(str1[j] != str2[i+j]) return false;
-        }
-        return true;
-    }
-
 public:
     string gcdOfStrings(string str1, string str2) {
-        if(str1.size() > str2.size()) swap(str1, str2);
-        this->str1 = str1;
-        this->str2 = str2;
-        n1 = str1.size();
-        n2 = str2.size();
-        int maxi = 0;
-        for(int k = 1; k<=n1+1; k++) if(isValid(k)) maxi = k;
-        return str1.substr(0, maxi);
+        if(str1 + str2 != str2 + str1) return "";
+        function<int(int, int)> gcd = [&](int a, int b){
+            if(b == 0) return a;
+            return gcd(b, a%b);
+        };
+        return str1.substr(0, gcd(str1.size(), str2.size()));
     }
 };
