24dbe0e11740b9335d771421894d4eb6b65745d8
Maaz Karim <103533417+Maaz-24503@users.noreply.github.com>
2025-04-13 14:33:14 +0500

    Time: 0 ms (100%), Space: 9.7 MB (93.98%) - LeetHub

--- 3243-count-the-number-of-powerful-integers/3243-count-the-number-of-powerful-integers.cpp
+++ 3243-count-the-number-of-powerful-integers/3243-count-the-number-of-powerful-integers.cpp
@@ -1,41 +1,43 @@
 class Solution {
 private:
     string s;
-    long long suff;
     int limit;
 
-    long long lessThan(long long lim){
-        if(lim < suff) return 0;
-        lim = lim < 0 ? 0 : lim;
-        string strlim = to_string(lim);
-        vector<vector<long long>> memo(2 ,vector<long long>(strlim.size()+1, -1));
-        function<long long(int,bool)> helper = [&](int i, bool strict){
-            if(i == strlim.size() - s.size()) {
-                return strict ? (s <= strlim.substr(i) ? 1LL : 0LL) : 1LL;
-            }
-            if(memo[strict][i] != -1) return memo[strict][i];
-            if(i == 0){
-                int poss = min(strlim[i] - '0', limit);
-                return memo[strict][i] = strlim[i] - '0' == poss ? (poss * helper(i+1, false)) + helper(i+1, true) :
-                                        (poss+1) * helper(i+1, false);
-            }
-            if(strict){
-                int poss = min(strlim[i] - '0', limit);
-                return memo[strict][i] = strlim[i] - '0' == poss ? (poss * helper(i+1, false)) + helper(i+1, true) :
-                                        (poss+1) * helper(i+1, false);
-            }
-            else return memo[strict][i] = (limit+1) * helper(i+1, false);
-        };
-        return helper(0, true);
+    long long toDec(string num) {
+        long long result = 0;
+        for (char c : num) {
+            int value = c - '0';
+            result = result * (limit+1) + value;    // if its dig 4, its base 5 bcz 0->4
+        }
+        return result+1;        // Count 0 for this base
     }
+
+    long long lessThan(long long lim){
+        if (lim < stoll(s)) return 0;
+        string strLim = to_string(lim);
+        int prefixLength = strLim.size() - s.size();
+        long long newLim = 0;
+        if (prefixLength > 0) newLim = stoll(strLim.substr(0, prefixLength));
+        if (strLim.substr(prefixLength) < s) newLim--;
+        strLim = to_string(newLim);
+        int i = 0;
+        while(i < strLim.size() && strLim[i] - '0' <= limit) i++;
+        if(i != strLim.size()){
+            while(i < strLim.size()){
+                strLim[i] = limit + '0';
+                i++;
+            }
+        }
+        return toDec(strLim);
+    }
+
 
 public:
     long long numberOfPowerfulInt(long long start, long long finish, int limit, string s) {
         int n = s.size();
         for(char& c: s) if(c > '0' + limit) return 0LL;
-        suff = stoll(s);
         this->limit = limit;
         this->s = s;
         return lessThan(finish) - lessThan(start-1);
-    }
+    } 
 };
