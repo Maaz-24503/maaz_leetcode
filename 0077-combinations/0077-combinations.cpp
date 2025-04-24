a0508d35ac8c34112d6194b92cc8bb78ed1369be
Maaz Karim <103533417+Maaz-24503@users.noreply.github.com>
2025-04-24 21:45:25 +0500

    Time: 488 ms (5%), Space: 297.6 MB (5.04%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,20 +1,23 @@
 class Solution {
 private:
     int n;
+    vector<unordered_map<int, vector<vector<int>>>> memo;
 
     vector<vector<int>> helper(int i, int k){
         if(k == 0) return vector<vector<int>> {{}};
         if(i > n) return vector<vector<int>> {};
+        if(memo[i].count(k)) return memo[i][k];
         vector<vector<int>> pick = helper(i + 1, k - 1);
         for(int j = 0; j<pick.size(); j++) pick[j].push_back(i);
         vector<vector<int>> notPick = helper(i + 1, k);
         for(int j = 0; j<notPick.size(); j++) pick.push_back(notPick[j]);
-        return pick;
+        return memo[i][k] = pick;
     }
 
 public:
     vector<vector<int>> combine(int n, int k) {
         this->n = n;
+        memo.resize(n + 1);
         return helper(1, k);
     }
 };
