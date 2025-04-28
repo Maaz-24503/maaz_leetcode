0db09d147e652535da3798d69c3d4f2999a012fc
Maaz Karim <103533417+Maaz-24503@users.noreply.github.com>
2025-04-28 16:44:25 +0500

    Time: 1 ms (72.61%), Space: 98.9 MB (76.24%) - LeetHub

--- 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.cpp
+++ 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.cpp
@@ -4,12 +4,12 @@ public:
         int n = nums.size();
         long long total = ((n+0LL)*(n+1LL))/2;
         long long bigger = 0, sum = 0;
-        int l = 0, r = 0;
-        while(l<n){
+        int r = 0;
+        for(int l = 0; l<n; l++){
             while(r < n && sum * (r-l) < k) sum += nums[r++];
             if(sum * (r-l) < k) break;
             bigger += (n - r + 1);
-            sum -= nums[l++];
+            sum -= nums[l];
         }
         return total - bigger;
     }
