450d91e8e069054fa5a158be44b45a55f86ff666
Maaz Karim <103533417+Maaz-24503@users.noreply.github.com>
2025-05-05 19:25:10 +0500

    Time: 0 ms (100%), Space: 9.4 MB (20.31%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
@@ -1,14 +1,12 @@
 class Solution {
-private:
-    int mod = 1e9 + 7;
 public:
     int numTilings(int n) {
-        vector<long long> f(n + 1, 0);
-        vector<long long> g(n + 1, 0);
-        f[0] = 1, f[1] = 1;
+        vector<long long> f(n + 1, 1);  // ending complete
+        vector<long long> g(n + 1, 0);  // ending with one empty
+        int Mod = 1e9 + 7;
         for(int i = 2; i<=n; i++){
-            g[i] = (g[i-1] + f[i-2]) % mod;
-            f[i] = (2*g[i - 1] + f[i - 1] + f[i - 2]) % mod;
+            f[i] = (f[i-1] + f[i-2] + 2*g[i-1]) % Mod;
+            g[i] = (f[i-2] + g[i-1]) % Mod;
         }
         return f[n];
     }
