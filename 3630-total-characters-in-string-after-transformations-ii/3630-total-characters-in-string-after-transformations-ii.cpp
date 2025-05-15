bece174c2a4631fb04631799fc09a94660228772
Maaz Karim <103533417+Maaz-24503@users.noreply.github.com>
2025-05-15 15:51:34 +0500

    Time: 442 ms (17.33%), Space: 174 MB (5.33%) - LeetHub

--- 3630-total-characters-in-string-after-transformations-ii/3630-total-characters-in-string-after-transformations-ii.cpp
+++ 3630-total-characters-in-string-after-transformations-ii/3630-total-characters-in-string-after-transformations-ii.cpp
@@ -1,58 +1,45 @@
 class Solution {
 public:
-    const int MOD = 1e9 + 7;
+    int MOD = 1e9 + 7;
     typedef vector<vector<long long>> Matrix;
+    Matrix identity;
 
-    Matrix multiply(Matrix &a, Matrix &b) {
-        int n = a.size();
-        Matrix res(n, vector<long long>(n, 0));
-        for (int i = 0; i < n; ++i)
-            for (int k = 0; k < n; ++k)
-                for (int j = 0; j < n; ++j)
-                    res[i][j] = (res[i][j] + a[i][k] * b[k][j]) % MOD;
-        return res;
+    Matrix matMul(Matrix matA, Matrix matB){
+        Matrix ans(26, vector<long long> (26, 0));
+        for(int i = 0; i<26; i++){
+            for(int k = 0; k<26; k++){
+                for(int j = 0; j<26; j++){
+                    ans[i][k] = (ans[i][k] + (matA[i][j] * matB[j][k])) % MOD;
+                }
+            }
+        }
+        return ans;
     }
 
-    Matrix matrixExpo(Matrix base, int exp) {
-        int n = base.size();
-        if (exp == 0) {
-            Matrix identity(n, vector<long long>(n, 0));
-            for (int i = 0; i < n; ++i) identity[i][i] = 1;
-            return identity;
-        }
-        Matrix half = matrixExpo(base, exp / 2);
-        Matrix result = multiply(half, half);
-
-        if (exp % 2 == 1) {
-            result = multiply(result, base);
-        }
-
-        return result;
+    Matrix matExpo(Matrix mat, int p){
+        if(p == 0) return identity;
+        Matrix div = matExpo(mat, p/2);
+        Matrix mul = matMul(div, div);
+        if(p % 2) return matMul(mul, mat);
+        else return mul;
     }
 
     int lengthAfterTransformations(string s, int t, vector<int>& nums) {
-        int n = 26;
-        Matrix M(n, vector<long long>(n, 0)); 
-
-        for (int i = 0; i < 26; ++i) {
-            for (int j = 1; j <= nums[i]; ++j) {
-                int to = (i + j) % 26;
-                M[to][i] = (M[to][i] + 1) % MOD;
+        Matrix transformation(26, vector<long long>(26, 0LL));
+        identity.resize(26, vector<long long>(26, 0LL));
+        for(int i = 0; i<26; i++) identity[i][i] = 1;
+        for(int i = 0; i<26; i++)
+            for(int j = 1; j<=nums[i]; j++)
+                transformation[(i + j) % 26][i]++;
+        vector<long long> freq(26, 0LL);
+        for(int i = 0; i<s.size(); i++) freq[s[i] - 'a']++;
+        Matrix finalTrans = matExpo(transformation, t);
+        long long ans = 0LL;
+        for(int i = 0; i<26; i++){
+            for(int j = 0; j<26; j++){
+                ans = (ans + (finalTrans[i][j] * freq[j])) % MOD;
             }
-        }
-
-        vector<long long> freq(26, 0);
-        for (char c : s) freq[c - 'a']++;
-
-        Matrix Mt = matrixExpo(M, t);
-
-        vector<long long> finalFreq(26, 0);
-        for (int i = 0; i < 26; ++i)
-            for (int j = 0; j < 26; ++j)
-                finalFreq[i] = (finalFreq[i] + Mt[i][j] * freq[j]) % MOD;
-
-        long long result = 0;
-        for (int i = 0; i < 26; ++i) result = (result + finalFreq[i]) % MOD;
-        return static_cast<int>(result);
+        } 
+        return ans;
     }
 };
