80b27f124fba32e01a2eeb5919a30b9b171a65fe
Maaz Karim <103533417+Maaz-24503@users.noreply.github.com>
2025-05-22 23:01:01 +0500

    Time: 0 ms (100%), Space: 72.1 MB (86.14%) - LeetHub

---
+++ 1206-corporate-flight-bookings/1206-corporate-flight-bookings.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        int m = bookings.size();
+        vector<int> ans(n, 0);
+        for(int i = 0; i<m; i++){
+            ans[bookings[i][0] - 1] += bookings[i][2]; 
+            if(bookings[i][1] < n) ans[bookings[i][1]] -= bookings[i][2]; 
+        }
+        for(int i = 1; i<n; i++) ans[i] += ans[i - 1];
+        return ans;
+    }
+};
