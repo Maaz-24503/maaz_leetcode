8f4a3f30bd4e6d347f50366ebe9ac87a76345491
Maaz Karim <103533417+Maaz-24503@users.noreply.github.com>
2025-05-30 17:21:04 +0500

    Time: 7 ms (65.87%), Space: 19.8 MB (37.93%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,23 +1,39 @@
 class Solution {
+private:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<pair<int, int>> mon;
+        int n = heights.size();
+        int ans = 0;
+        for(int i = 0; i<n; i++){
+            while(!mon.empty() && mon.top().first > heights[i]){
+                auto [height, ind] = mon.top();
+                mon.pop();
+                int width = mon.empty() ? i : i - mon.top().second - 1;
+                ans = max(ans, height * width);
+            }
+            mon.push({heights[i], i});
+        }
+        int end = 0;
+        if(!mon.empty()) end = mon.top().first;
+        while(!mon.empty()){
+            auto [height, ind] = mon.top();
+            mon.pop();
+            int width = mon.empty() ? n : n - mon.top().second - 1;
+            ans = max(ans, height * width);
+        }
+        return ans;
+    }
+
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
         int n = matrix.size(), m = matrix[0].size();
-        vector<vector<int>> dp (n, vector<int> (m, 0));
-        for(int i = 0; i<n; i++)
-            for(int j = 0; j<m; j++)
-                if(matrix[i][j] == '1') dp[i][j] = 1;
-        for(int i = 0; i<n; i++)
-            for(int j = m - 2; j>=0; j--)
-                dp[i][j] = matrix[i][j] == '1' ? dp[i][j + 1] + 1 : 0;
+        vector<int> dp(m, 0);
         int ans = 0;
         for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                int mini = INT_MAX;
-                for(int k = i; k<n; k++){
-                    mini = min(mini, dp[k][j]);
-                    ans = max(ans, mini * (k - i + 1));
-                }
-            }
+            for(int j = 0; j<m; j++)
+                if(matrix[i][j] == '1') dp[j]++;
+                else dp[j] = 0;
+            ans = max(ans, largestRectangleArea(dp));
         }
         return ans;
     }
