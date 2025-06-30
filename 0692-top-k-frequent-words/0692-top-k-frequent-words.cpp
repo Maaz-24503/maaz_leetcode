280f4644062b3b2cf45968ea2f2236ab3a91be63
Maaz Karim <103533417+Maaz-24503@users.noreply.github.com>
2025-06-30 12:50:59 +0500

    Time: 8 ms (12.91%), Space: 18.9 MB (5.64%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -2,16 +2,21 @@ class Solution {
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
         unordered_map<string, int> freq;
-        int n = words.size();
-        for(string &word : words) freq[word]++;
-        vector<pair<int, string>> temp;
-        for(auto [word, f] : freq) temp.push_back({f, word});
-        sort(temp.begin(), temp.end(), [](const auto &a, const auto &b){
-            if(a.first != b.first) return a.first > b.first;
-            return a.second < b.second;
-        });
+        for (string &word : words) freq[word]++;
+        function<bool(const pair<int, string>, const pair<int, string>)> comp = [](const pair<int, string> &a, const pair<int, string> &b) -> bool {
+            if (a.first != b.first) return a.first > b.first; 
+            return a.second < b.second; 
+        };
+        priority_queue<pair<int, string>, vector<pair<int, string>>, decltype(comp)> pq(comp);
+        for (auto &[word, f] : freq) {
+            pq.push({f, word});
+            if (pq.size() > k) pq.pop();
+        }
         vector<string> ans(k);
-        for(int i = 0; i<k; i++) ans[i] = temp[i].second;
+        for (int i = k - 1; i >= 0; i--) {
+            ans[i] = pq.top().second;
+            pq.pop();
+        }
         return ans;
     }
 };
