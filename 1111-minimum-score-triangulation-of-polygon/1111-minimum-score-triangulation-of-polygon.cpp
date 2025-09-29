0f9770d8ef1afa0c7496c3bcda6d4185527838ad
Maaz Karim <103533417+Maaz-24503@users.noreply.github.com>
2025-09-29 21:47:16 +0500

    Time: 7 ms (5.34%), Space: 12.3 MB (6.21%) - LeetHub

--- 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.cpp
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
     int minScoreTriangulation(vector<int>& values) {
         int n = values.size();
-        vector<vector<int>> dp(n, vector<int> (n, -1));
-        function<int(int, int)> helper = [&](int i, int j){
+        vector<vector<int>> memo(n, vector<int> (n, -1));
+        function<int(int, int)> helper = [&](int i, int j) -> int {
             if(abs(i - j) <= 1) return 0;
-            if(dp[i][j] != -1) return dp[i][j];
+            if(memo[i][j] != -1) return memo[i][j];
             int ans = INT_MAX;
-            for(int k = i + 1; k<j; k++)
-                ans = min(ans, helper(i, k) + (values[i] * values[k] * values[j]) + helper(k, j));
-            return dp[i][j] = ans;
+            for(int k = i + 1; k < j; k++){
+                // left subshape + current triangle + right subshape
+                int leftShape = helper(i, k);
+                int currTriangle = values[i] * values[j] * values[k];
+                int rightShape = helper(k, j);
+                ans = min(ans, leftShape + currTriangle + rightShape);
+            }
+            return memo[i][j] = ans;
         };
         return helper(0, n - 1);
     }
