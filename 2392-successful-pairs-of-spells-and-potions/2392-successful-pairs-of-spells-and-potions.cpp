1df42edcd989f92a2e7cb3c566eae40acd9066cb
Maaz Karim <103533417+Maaz-24503@users.noreply.github.com>
2025-10-08 02:30:08 -0700

    Time: 55 ms (12.29%), Space: 125.1 MB (65.71%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
         int n = spells.size(), m = potions.size();
-        sort(potions.begin(), potions.end());
-        vector<int> ans(n);
-        function<int(long long)> bs = [&](long long target){
-            int l = 0, r = m-1;
-            int ans = m;
-            while(l<=r){
-                int mid = l + (r - l)/2;
-                if(potions[mid] < target) l = mid+1;
-                else{
+        function<int(int)> lowerBound = [&](int spell) -> int {
+            int l = 0, r = m - 1, ans = m;
+            while(l <= r){
+                int mid = (l + r) / 2;
+                if((spell + 0LL) * potions[mid] < success) l = mid + 1;
+                else {
                     r = mid - 1;
                     ans = mid;
                 }
             }
-            return m - ans;
+            return ans;
         };
-        for(int i = 0; i<n; i++){
-            long long target = (success + spells[i] - 1LL) / spells[i];
-            ans[i] = bs(target);
+        sort(potions.begin(), potions.end());
+        vector<int> ans(n);
+        for(int i = 0; i < n; i++){
+            ans[i] = m - lowerBound(spells[i]);
         }
         return ans;
     }
