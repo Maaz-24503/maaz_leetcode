f9d64b2fee913bdc502f9f634d479ce653526a97
Maaz Karim <103533417+Maaz-24503@users.noreply.github.com>
2025-11-26 02:47:34 -0800

    Time: 139 ms (9.67%), Space: 54.7 MB (6.24%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -3,13 +3,13 @@ public:
     int maxDistance(vector<vector<int>>& grid) {
         int n = grid.size();
         vector<vector<int>> dist(n, vector<int> (n, INT_MAX));
-        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
+        queue<vector<int>> q;
         
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++) {
                 if(grid[i][j] == 1){
                     dist[i][j] = 0;
-                    pq.push({0, i, j});
+                    q.push({0, i, j});
                 }
             }
         }
@@ -20,16 +20,16 @@ public:
 
         vector<vector<int>> d{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
 
-        while(!pq.empty()){
-            vector<int> curr = pq.top();
-            pq.pop();
+        while(!q.empty()){
+            vector<int> curr = q.front();
+            q.pop();
             int currDist = curr[0], curri = curr[1], currj = curr[2];
             for(int k = 0; k < 4; k++){
                 int ni = curri + d[k][0], nj = currj + d[k][1];
                 if(isValidWater(ni, nj)){
                     if(1 + currDist < dist[ni][nj]){
                         dist[ni][nj] = 1 + currDist;
-                        pq.push({1 + currDist, ni, nj});
+                        q.push({1 + currDist, ni, nj});
                     }
                 }
             }
