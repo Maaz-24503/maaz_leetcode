c4a0dea3e74fe5775847f9e05a6336a093e77382
Maaz Karim <103533417+Maaz-24503@users.noreply.github.com>
2026-01-10 00:20:00 +0300

    Time: 92 ms (11.06%), Space: 31.8 MB (5.96%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -5,30 +5,36 @@ public:
     // 2a = x + s
     // a = (x + s) / 2  -->  Final
     int stoneGameII(vector<int>& piles) {
-        unordered_map<string, int> memo;
         int n = piles.size();
+        
+        vector<vector<vector<int>>> memo(
+            n + 1,
+            vector<vector<int>>(n + 1, vector<int>(2, INT_MIN))
+        );
+
         function<int(int, int, bool)> dp = [&](int i, int m, bool turn) -> int {
-            if(i >= n) return 0;
-            string key = to_string(i) + " " + to_string(m) + (turn ? "t" : "f");
-            if(memo.find(key) != memo.end()) return memo[key];
-            if(turn){
+            if (i >= n) return 0;
+            if (memo[i][m][turn] != INT_MIN) return memo[i][m][turn];
+
+            if (turn) {
                 int ans = INT_MIN, sum = 0;
-                for(int x = 0; x < min(2*m, n - i); x++){
+                for (int x = 0; x < min(2 * m, n - i); x++) {
                     sum += piles[x + i];
                     ans = max(ans, sum + dp(x + i + 1, max(x + 1, m), !turn));
                 }
-                return memo[key] = ans;
+                return memo[i][m][turn] = ans;
             } else {
                 int ans = INT_MAX, sum = 0;
-                for(int x = 0; x < min(2*m, n - i); x++){
+                for (int x = 0; x < min(2 * m, n - i); x++) {
                     sum += piles[x + i];
                     ans = min(ans, -sum + dp(x + i + 1, max(x + 1, m), !turn));
                 }
-                return memo[key] = ans;
+                return memo[i][m][turn] = ans;
             }
         };
+
         int sum = 0;
-        for(int i = 0; i < n; i++) sum += piles[i];
+        for (int i = 0; i < n; i++) sum += piles[i];
         return (dp(0, 1, true) + sum) / 2;
     }
 };
