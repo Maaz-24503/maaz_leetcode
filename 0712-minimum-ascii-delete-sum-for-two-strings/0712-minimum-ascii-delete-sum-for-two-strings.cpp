31313524176eeb550e6fe698b71c23ef872a60e6
Maaz Karim <103533417+Maaz-24503@users.noreply.github.com>
2026-01-10 21:12:03 +0300

    Time: 15 ms (74.15%), Space: 18.1 MB (76.32%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -2,22 +2,25 @@ class Solution {
 public:
     int minimumDeleteSum(string s1, string s2) {
         int n = s1.size(), m = s2.size();
-        vector<vector<int>> memo(n + 1, vector<int> (m + 1, -1));
-        function<int(int, int)> dp = [&](int i, int j) -> int {
-            if(memo[i][j] != -1) return memo[i][j];
-            if(i >= n) {
-                memo[i][j] = 0;
-                for(int k = j; k < m; k++) memo[i][j] += s2[k]; 
-                return memo[i][j];
+        vector<vector<int>> memo(n + 1, vector<int> (m + 1, 0));
+        int sum = 0;
+        for(int i = n - 1; i>=0; i--) {
+            sum += s1[i];
+            memo[i][m] = sum;
+        } 
+        sum = 0;
+        for(int i = m - 1; i>=0; i--) {
+            sum += s2[i];
+            memo[n][i] = sum;
+        } 
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = m - 1; j >= 0; j--){
+                if(s1[i] == s2[j]) memo[i][j] = memo[i + 1][j + 1];
+                else{
+                    memo[i][j] = min(s1[i] + memo[i + 1][j], s2[j] + memo[i][j + 1]);
+                }
             }
-            if(j >= m) {
-                memo[i][j] = 0;
-                for(int k = i; k < n; k++) memo[i][j] += s1[k]; 
-                return memo[i][j];
-            }
-            if(s1[i] == s2[j]) return memo[i][j] = dp(i + 1, j + 1);
-            return memo[i][j] = min(s1[i] + dp(i + 1, j), s2[j] + dp(i, j + 1));
-        };
-        return dp(0, 0);
+        }
+        return memo[0][0];
     }
 };
