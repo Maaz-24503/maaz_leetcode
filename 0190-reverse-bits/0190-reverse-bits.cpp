dc722a035b19bb00994daccc76275159802f0be0
Maaz Karim <103533417+Maaz-24503@users.noreply.github.com>
2026-02-16 09:43:23 +0300

    Time: 0 ms (100%), Space: 8.3 MB (46.07%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int reverseBits(int n) {
-        string num = "";
-        for(int i = 0; i < 32; i++){
-            if((n & 1) == 1) num += "1";
-            else num += "0";
-            n >>= 1;
-        }
-        cout<<num;
         int ans = 0;
-        for(int i = 0; i < 32; i++) {
+        for(int i = 0; i < 32; i++){
             ans <<= 1;
-            if(num[i] == '1') ans |= 1;
+            if((n & 1) == 1) ans |= 1;
+            n >>= 1;
         }
         return ans;
     }
