73b8cfd730a937f65323dc0c5f52b40646ec0297
Maaz Karim <103533417+Maaz-24503@users.noreply.github.com>
2026-08-09 09:55:34 +0300

    Time: 87 ms (8.49%), Space: 31.4 MB (6.2%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
-    // a + b = s
-    // x = a - b
-    // 2a = x + s
-    // a = (x + s) / 2  -->  Final
     int stoneGameII(vector<int>& piles) {
         int n = piles.size();
-        
-        vector<vector<vector<int>>> memo(
-            n + 1,
-            vector<vector<int>>(n + 1, vector<int>(2, INT_MIN))
-        );
-
+        vector<vector<vector<int>>> memo (n, vector<vector<int>> (n + 1, vector<int>(2, -1)));
         function<int(int, int, bool)> dp = [&](int i, int m, bool turn) -> int {
-            if (i >= n) return 0;
-            if (memo[i][m][turn] != INT_MIN) return memo[i][m][turn];
-
-            if (turn) {
-                int ans = INT_MIN, sum = 0;
-                for (int x = 0; x < min(2 * m, n - i); x++) {
-                    sum += piles[x + i];
-                    ans = max(ans, sum + dp(x + i + 1, max(x + 1, m), !turn));
+            if(i >= n) return 0;
+            if(memo[i][m][turn] != -1) return memo[i][m][turn];
+            int maxM = min(n - i, 2*m);
+            if(turn){
+                int currSum = 0, ans = 0;
+                for(int k = 1; k <= maxM; k++){
+                    currSum += piles[i + k - 1];
+                    ans = max(ans, currSum + dp(i + k, max(k, m), !turn));
                 }
-                return memo[i][m][turn] = ans;
+                memo[i][m][turn] = ans;
             } else {
-                int ans = INT_MAX, sum = 0;
-                for (int x = 0; x < min(2 * m, n - i); x++) {
-                    sum += piles[x + i];
-                    ans = min(ans, -sum + dp(x + i + 1, max(x + 1, m), !turn));
+                int ans = INT_MAX;
+                for(int k = 1; k <= maxM; k++){
+                    ans = min(ans, dp(i + k, max(k, m), !turn));
                 }
-                return memo[i][m][turn] = ans;
+                memo[i][m][turn] = ans;
             }
+            return memo[i][m][turn];
         };
-
-        int sum = 0;
-        for (int i = 0; i < n; i++) sum += piles[i];
-        return (dp(0, 1, true) + sum) / 2;
+        return dp(0, 1, true);
     }
 };
