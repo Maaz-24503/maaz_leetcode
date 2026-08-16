a44b5cbb4830c59f40ddd29e4bac97f9a8f6d1cc
Maaz Karim <103533417+Maaz-24503@users.noreply.github.com>
2026-08-16 09:59:36 +0300

    Time: 7 ms (39.59%), Space: 131.4 MB (33.5%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        vector<int> f(3, 0);
+        for(int &s : stones) f[s % 3]++;
+        if(f[0] % 2 == 0) return f[1] && f[2];
+        return abs(f[1] - f[2]) > 2;
+    }
+};
